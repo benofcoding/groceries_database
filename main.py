5022cc1c8196178037ea8d5209bab2db02915a08
benofcoding <166450432+benofcoding@users.noreply.github.com>
2024-05-27 16:02:36 +1200

    i added the base code to build from

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+import sqlite3
+
+DATABASE = 'groceries.db'
+
+def print_main_table():
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM groceries"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for grocerie in results:
+        print(f"{grocerie[0]:<5}{grocerie[1]:<65}{grocerie[2]}")
+    db.close()
+
+print_main_table()
