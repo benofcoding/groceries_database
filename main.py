18759d3bea7d63c57cd5519ca57071b85fcb66cc
benofcoding <166450432+benofcoding@users.noreply.github.com>
2024-05-28 13:31:35 +1200

    i formated the output

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ def print_main_table():
     sql = "SELECT * FROM groceries"
     cursor.execute(sql)
     results = cursor.fetchall()
+    print(" id    Name                                                                Brand  CPS     servings   GPS     cost    type")
     for grocerie in results:
-        print(f"{grocerie[0]:<5}{grocerie[1]:<65}{grocerie[2]}")
+        print(f"| {grocerie[0]:<2} | {grocerie[1]:<65} | {grocerie[2]:<4} | {grocerie[3]:<5} | {grocerie[4]:<8} | {grocerie[5]:<5} | {grocerie[6]:<5} | {grocerie[7]:<5} |")
     db.close()
 
 print_main_table()
