32e85f244ce301ce4e3335585ea5cf01bd8cc031
benofcoding <166450432+benofcoding@users.noreply.github.com>
2024-06-05 12:58:30 +1200

    I added an input to search for a specific item in the database

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import sqlite3
 
+
+#constants
 DATABASE = 'groceries.db'
 
-def print_main_table():
+def Print_name_ID():
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
-    sql = "SELECT * FROM groceries"
+    sql = "SELECT Groceries.ID, Groceries.Name FROM Groceries"
     cursor.execute(sql)
     results = cursor.fetchall()
-    print(" id    Name                                                                Brand  CPS     servings   GPS     cost    type")
+    print("  ID   Name")
     for grocerie in results:
-        print(f"| {grocerie[0]:<2} | {grocerie[1]:<65} | {grocerie[2]:<4} | {grocerie[3]:<5} | {grocerie[4]:<8} | {grocerie[5]:<5} | {grocerie[6]:<5} | {grocerie[7]:<5} |")
+        print(f"| {grocerie[0]:<2} | {grocerie[1]:<65} |")
+
+def search_for_grocerie():
+    Print_name_ID()
+    print("")
+    ID = input("Input the ID of the grocerie you would like to see more information about.")
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = f"SELECT Groceries.Name, Groceries.price_c, Groceries.Serving_Size_g, Groceries.Calories_Per_Serving, Groceries.Servings, Product_Type.Type, Brand.Name FROM Groceries JOIN Brand on Brand.ID = Groceries.Brand_ID JOIN Product_Type on Product_Type.ID = Groceries.Product_Type_ID WHERE Groceries.ID LIKE {ID}"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    print(results)
     db.close()
 
-print_main_table()
+
+
+search_for_grocerie()
