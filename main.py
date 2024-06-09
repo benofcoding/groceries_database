2ead3696a17e43223ac3a7c6b0a575be7897186e
benofcoding <166450432+benofcoding@users.noreply.github.com>
2024-06-09 20:09:48 +1200

    added an option to sort by asc or desc for order by function

--- main.py
+++ main.py
@@ -10,15 +10,12 @@ Calories_Per_Serving_Length = 3
 Servings_Length = 2
 Serving_Size_Length = 3
 Price_length = 4
-
 Brand_ID_Length = 2
 Brand_Name_Length = 15
-
 Product_Type_ID_Length = 2
 Product_Type_Length = 30
 
-
-
+#all functions
 def Print_name_and_ID_for_Groceries():
     #establish interface
     db = sqlite3.connect(DATABASE)
@@ -31,8 +28,6 @@ def Print_name_and_ID_for_Groceries():
     print("  ID   Name")
     for grocery in results:
         print(f"| {grocery[0]:<{Product_ID_Length}} | {grocery[1]:<{Product_Name_Length}} |")
-
-
 
 def search_for_grocery():
     #show name and id table
@@ -80,10 +75,6 @@ def search_for_grocery():
             #if the id inputed wasnt an integer print string:
             print("Invalid input")
 
-
-
-
-
 def ORDER_BY():
     options = {"1":"Groceries.price_c",
                         "2":"Groceries.Serving_Size_g",
@@ -103,15 +94,20 @@ def ORDER_BY():
     cursor = db.cursor()
 
     while True:
-        try:
-            Sort_by = input("either input the ID of what you would like to sort by or input 'back' to go back.\n")
-            if Sort_by == "back":
-                main_menu()
+        Sort_by = input("either input the ID of what you would like to sort by or input 'back' to go back.\n")
+        if Sort_by == "back":
+            main_menu()
+        elif Sort_by == "1" or Sort_by == "2" or Sort_by == "3" or Sort_by == "4":
+            break
+        else:
+            print("that is an invalid input")
 
-            sql = f"SELECT Groceries.Name, Groceries.price_c, Groceries.Serving_Size_g, Groceries.Calories_Per_Serving, Groceries.Servings, Product_Type.Type, Brand.Name FROM Groceries JOIN Brand on Brand.ID = Groceries.Brand_ID JOIN Product_Type on Product_Type.ID = Groceries.Product_Type_ID ORDER BY {options[Sort_by]} DESC"
+    while True:
+        ASC_DESC = input("do you want to sort by ascending or descending? ASC/DESC\n")
+        if ASC_DESC == "ASC" or ASC_DESC == "DESC":
+            sql = f"SELECT Groceries.Name, Groceries.price_c, Groceries.Serving_Size_g, Groceries.Calories_Per_Serving, Groceries.Servings, Product_Type.Type, Brand.Name FROM Groceries JOIN Brand on Brand.ID = Groceries.Brand_ID JOIN Product_Type on Product_Type.ID = Groceries.Product_Type_ID ORDER BY {options[Sort_by]} {ASC_DESC}"
             cursor.execute(sql)
             results = cursor.fetchall()
-
             
             Acronmys = ["P(c) = Price in cents",
                         "SS = Servin size in grams",
@@ -127,12 +123,8 @@ def ORDER_BY():
                 print(f"| {result[0]:<{Product_Name_Length}} | {result[1]:<{Price_length}} | {result[2]:<{Serving_Size_Length}} | {result[3]:<{Calories_Per_Serving_Length}} | {result[4]:<{Servings_Length}} | {result[5]:<{Product_Type_Length}} | {result[6]:<{Brand_Name_Length}} |")
             print("-"*144)
             db.close()
-        except:
+        else:
             print("Invalid input")
-
-
-
-
 
 def show_greaterthan_or_smallerthan():
     options = {"1":"Groceries.price_c",
@@ -144,8 +136,6 @@ def show_greaterthan_or_smallerthan():
                     "| 2. | Serving_size         |",
                     "| 3. | Calorise per serving |",
                     "| 4. | Sevings              |"]
-
-
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
     while True:
@@ -161,7 +151,6 @@ def show_greaterthan_or_smallerthan():
         else:
             print("that is not a valid input")
             print("")
-
     while True:  
         operator = int(input("Would you like to sort by greater than or less than?, input 1 or 2. If you want to go back to main menu input 'back'.\n"))
         if operator == "back":
@@ -169,8 +158,7 @@ def show_greaterthan_or_smallerthan():
         elif int(operator) == 1 or operator == 2:
             break
         else:
-            print("that is not a valid input")
-                
+            print("that is not a valid input")       
     while True:
         try:
             value = int(input("what value would you like to sort by? If you want to go back to main menu input 'back'.\n"))
@@ -180,8 +168,6 @@ def show_greaterthan_or_smallerthan():
             cursor.execute(sql)
             results = cursor.fetchall()
             print(options[str(column)])
-            
-            
             Acronmys = ["P(c) = Price in cents",
                         "SS = Servin size in grams",
                         "CPS = Calories per serving",
@@ -198,22 +184,13 @@ def show_greaterthan_or_smallerthan():
             print("")
             db.close()
             break
-        
         except:
             print("that is not a valid input")
-        
-
-
-            
-       
-
-
 
 
 
 def main_menu():
     while True:
-
         all_options = {"1":search_for_grocery,
                        "2":ORDER_BY,
                        "3":show_greaterthan_or_smallerthan}
@@ -221,20 +198,16 @@ def main_menu():
                             "| 2.    | sort by a specific data type                    |",
                             "| 3.    | sort any column by greater than or smaller than |",
                             "| quit. | Quit the program                                |"]
-        
         for option in all_options_text:
             print(option)
-        
         try:
-            
             option_chosen = input("what would you like to do?\n") 
             if str(option_chosen) in all_options:    
                 all_options[option_chosen]()
             elif str(option_chosen).lower() ==  "quit." or "quit":
                 break
             else:
-                print("that is not an option")
-                
+                print("that is not an option")  
         except:
             print("that is not an option")
 
